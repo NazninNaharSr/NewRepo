1fe112120e970ed3b7a1af6fc201f5ae014b88a1
iit <iit@DESKTOP-VPAKTSG>
2022-03-28 15:30:00 +0600

    Add project files.

---
+++ UserService.asmx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace UserService
+{
+    /// <summary>
+    /// Summary description for UserService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class UserService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string UserInformation()
+        {
+            return "Hello World";
+        }
+        [WebMethod]
+        public string AddUser()
+        {
+            return "Added Successfully";
+        }
+
+        [WebMethod]
+        public string UpdateUser()
+        {
+            return "Updated Successfully";
+        }
+
+        [WebMethod]
+        public string DeleteUser()
+        {
+            return "Deleted Successfully";
+        }
+        [WebMethod]
+        public string ViewMenu()
+        {
+            return "View Menu List";
+        }
+    }
+}
